1b5fcc2c0338ff42dada97fa00faee60fc63c511
Vilislav <vilislavangelov@gmail.com>
2024-10-29 18:10:14 +0200

    did the basic operators and now the calculator
    works with addition, subtraction, multiplication
    and division and outputs the the result on the
    screen.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 //TODO
-//add, subtract, multiply, divide
-//create variables for num1, opearator and num2
-//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-//create html calculator, add a clear button
 //functions to populate the disaplay 
 //make the calc work
 //opearate()
@@ -15,3 +11,56 @@
 //Let them type in decimals, the . can only be pressed once per number
 //add a backspace button
 //add KEYBOARD SUPPORT
+
+let num1;
+let num2;
+let operator;
+
+const basicOperators = ['+', '-', 'x', '/'];
+const display = document.getElementById("display");
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(num1, num2, operator) {
+
+    switch(operator) {
+        case '+':
+            display.value = add(num1, num2);
+            break;
+        case '-':
+            display.value = subtract(num1, num2);
+            break;
+        case 'x':
+            display.value = multiply(num1, num2);
+            break;
+        case '/':
+            display.value = divide(num1, num2);
+            break;
+    }
+}
+
+document.addEventListener("click", function(e) {
+    display.value += e.target.textContent;
+    if(e.target.id === "btn-clear"){
+        display.value = "";
+    }
+
+    if(e.target.id === 'btn-equals') {
+        let expr = display.value.split("");
+        for(let i = 0; i < expr.length; i++) {
+            if(basicOperators.includes(expr[i])){
+                num1 = expr.splice(0, i).join("");
+                operator = expr.splice(0, 1).toString();
+                i = 0;
+            }
+            if(expr[i] == '='){
+                num2 = expr.splice(0, i).join("");
+            }
+        }
+        operate(parseInt(num1), parseInt(num2), operator)
+    }
+})
+
