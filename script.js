5f1896b7f92b9dd265236439b9be13d8a4e0775b
Vilislav <vilislavangelov@gmail.com>
2024-10-29 07:04:42 +0200

    Did the basic HTML structure of the calculator.

---
+++ script.js
@@ -0,0 +1,17 @@
+//TODO
+//add, subtract, multiply, divide
+//create variables for num1, opearator and num2
+//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+//create html calculator, add a clear button
+//functions to populate the disaplay 
+//make the calc work
+//opearate()
+//Your calculator should not evaluate more than a single pair of numbers at a time.
+//round answers with long decimals so that they don’t overflow the screen.
+//Pressing “clear” should wipe out any existing data. Make sure the user is really starting fresh after pressing “clear”
+//Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
+
+//BONUS
+//Let them type in decimals, the . can only be pressed once per number
+//add a backspace button
+//add KEYBOARD SUPPORT
