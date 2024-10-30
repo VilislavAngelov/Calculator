b618b01ef41169b74ffc416f6f566a53a199d141
Vilislav <vilislavangelov@gmail.com>
2024-10-30 17:49:51 +0200

    was able to get the calculator to work with negative numbers and refactored how the calculator stores numbers. I think it's working a lot better now but we still have a long way to go.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //TODO
 //round answers with long decimals so that they don’t overflow the screen.
-//Pressing “clear” should wipe out any existing data. Make sure the user is really starting fresh after pressing “clear”
 //Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
 
 //BONUS
@@ -8,9 +7,10 @@
 //add a backspace button
 //add KEYBOARD SUPPORT
 
-let num1;
-let num2;
-let operator;
+let num1 = '';
+let num2 = '';
+let operator = null;
+let expectingNum2 = false;
 
 const basicOperators = ["+", "-", "x", "/"];
 const operatorIds = ["btn-plus", "btn-minus", "btn-multiply", "btn-divide"];
@@ -41,56 +41,50 @@ function operate(num1, num2, operator) {
 
 keyboard.addEventListener("click", function (e) {
   if (e.target.classList.contains("calc-btn")) {
-    display.value += e.target.textContent;
-    if (e.target.id === "btn-clear") {
-      display.value = "";
-      num1 = undefined;
-      num2 = undefined;
-      operator = undefined;
-    }
+    const key = e.target.textContent
 
+    display.value += key;
+    if (!isNaN(key) || key === '.' || (key === '-' && !expectingNum2 && num1 === '') || (key === '-' && expectingNum2 && num2 === '')) {
+        if (!expectingNum2) {
+          num1 += key;
+        } else {
+          num2 += key;
+        }
+        return;
+      }
+    
+      if (!(num1 == '') && basicOperators.includes(key)) {
+        operator = key;
+        expectingNum2 = true;
+        return;
+      }
+    
     if (operatorIds.includes(e.target.id)) {
-      if (operator != undefined) {
-        let expr = display.value.split("");
-        exprEval = expr.splice(0, expr.length - 1);
-        for (let i = 0; i < exprEval.length; i++) {
-          if (basicOperators.includes(exprEval[i])) {
-            num1 = exprEval.splice(0, i).join("");
-            operator = exprEval.splice(0, 1).toString();
-            i = 0;
-            num2 = exprEval.splice(0, exprEval.length);
-          }
+      if (operator != null) {
           operate(parseInt(num1), parseInt(num2), operator);
           num1 = parseInt(display.value);
-          operator = expr.join("");
-          num2 = undefined;
+          num2 = '';
           display.value += operator;
-        }
       }
-      operator = e.target.id;
     }
 
-    if (e.target.id === "btn-equals") {
-      let expr = display.value.split("");
-      for (let i = 0; i < expr.length; i++) {
-        if (basicOperators.includes(expr[i])) {
-          num1 = expr.splice(0, i).join("");
-          operator = expr.splice(0, 1).toString();
-          i = 0;
-        }
-        if (expr[i] == "=") {
-          num2 = expr.splice(0, i).join("");
-        }
-      }
+    if (e.target.id === "btn-equals" && (num1 != '' && num2 != '' && operator != null)) {
       operate(parseInt(num1), parseInt(num2), operator);
       num1 = parseInt(display.value);
-      operator = undefined;
-      num2 = undefined;
+      operator = null;
+      num2 = '';
     }
 
     if(e.target.id === "btn-backspace" && display.value.length > 0) {
         let newDisplay = display.value.split("");
         display.value = newDisplay.splice(0, display.value.length - 2).join("");
     }
+
+    if (e.target.id === "btn-clear") {
+        display.value = "";
+        num1 = '';
+        num2 = '';
+        operator = null;
+      }
   }
 });
