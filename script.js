6da6e21d9ebe5d65f524629ba089b823de9403e0
Vilislav <vilislavangelov@gmail.com>
2024-10-31 06:27:56 +0200

    Made the calculator work with decimal points and percent values and cleaned up some errors;

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let num1 = '';
 let num2 = '';
 let operator = null;
 let expectingNum2 = false;
+let isNum1Perc = false;
+let isNum2Perc = false;
 
 const basicOperators = ["+", "-", "x", "/"];
 const operatorIds = ["btn-plus", "btn-minus", "btn-multiply", "btn-divide"];
@@ -21,6 +23,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const percent = (a = 1, b) => (a * b) / 100;
 
 function operate(num1, num2, operator) {
   switch (operator) {
@@ -44,37 +47,59 @@ keyboard.addEventListener("click", function (e) {
     const key = e.target.textContent
 
     display.value += key;
-    if (!isNaN(key) || key === '.' || (key === '-' && !expectingNum2 && num1 === '') || (key === '-' && expectingNum2 && num2 === '')) {
+    if (!isNaN(key) || key === '.' || key === '%'  || (key === '-'  && !expectingNum2 && num1 === '') || (key === '-' && expectingNum2 && num2 === '')) {
         if (!expectingNum2) {
           num1 += key;
+          if(key === '%') {
+            isNum1Perc = true;
+          }
         } else {
           num2 += key;
+          if(key === '%') {
+            isNum2Perc = true;
+          }
         }
-        return;
       }
     
       if (!(num1 == '') && basicOperators.includes(key)) {
         operator = key;
         expectingNum2 = true;
-        return;
       }
+
+    if(num1 != '' && num2 != '' && operator != null) {
+        if (operatorIds.includes(e.target.id)) {
+            if (operator != null) {
+                if(isNum1Perc) {
+                    num1 = percent(parseFloat(num1))
+                }
+                if(isNum2Perc) {
+                    num2 = percent(parseFloat(num1), parseFloat(num2));
+                }
+                operate(parseFloat(num1), parseFloat(num2), operator);
+                num1 = parseFloat(display.value);
+                num2 = '';
+                isNum1Perc = false;
+                isNum2Perc = false;
+                display.value += operator;
+            }
+          }
+      
+          if (e.target.id === "btn-equals") {
+            if(isNum1Perc) {
+                num1 = percent(parseFloat(num1))
+            }
+            if(isNum2Perc) {
+                num2 = percent(parseFloat(num1), parseFloat(num2));
+            }
+            operate(parseFloat(num1), parseFloat(num2), operator);
+            num1 = parseFloat(display.value);
+            operator = null;
+            num2 = '';
+            isNum1Perc = false;
+            isNum2Perc = false;
+          }
+    }
     
-    if (operatorIds.includes(e.target.id)) {
-      if (operator != null) {
-          operate(parseInt(num1), parseInt(num2), operator);
-          num1 = parseInt(display.value);
-          num2 = '';
-          display.value += operator;
-      }
-    }
-
-    if (e.target.id === "btn-equals" && (num1 != '' && num2 != '' && operator != null)) {
-      operate(parseInt(num1), parseInt(num2), operator);
-      num1 = parseInt(display.value);
-      operator = null;
-      num2 = '';
-    }
-
     if(e.target.id === "btn-backspace" && display.value.length > 0) {
         let newDisplay = display.value.split("");
         display.value = newDisplay.splice(0, display.value.length - 2).join("");
@@ -85,6 +110,9 @@ keyboard.addEventListener("click", function (e) {
         num1 = '';
         num2 = '';
         operator = null;
+        expectingNum2 = false;
+        isNum1Perc = false;
+        isNum2Perc = false;
       }
   }
 });
